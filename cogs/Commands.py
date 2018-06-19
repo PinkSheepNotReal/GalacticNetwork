3ce691790a001e240628562c13395888ee8ef9b9
PinkSheepNotReal <33484537+PinkSheepNotReal@users.noreply.github.com>
2018-06-19 19:52:51 +0300

    Update Commands.py

--- cogs/Commands.py
+++ cogs/Commands.py
@@ -157,7 +157,7 @@ class MemberCommands():
     @commands.command()
     async def botinfo(self, ctx):
         info = discord.Embed(title="Galactic Network stats", color=0xff8080)
-        info.add_field(name="servers", value="Servers: {}".format(bot.servers))
+        info.add_field(name="servers", value="Servers: {}".format(self.bot.servers))
         info.add_footer(text="this command is in development")
         await ctx.send(embed=info)
         
